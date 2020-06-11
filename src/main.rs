87f0d8a14657cf0ff3abdc5903ccca62ccc1d366
iinc0gnit0 <iinc0gnit0@pm.me>
2020-06-11 07:10:07 -0500

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 // Dependencies
-use isahc;
+use isahc; // 0.9.3
+use regex::Regex; // 1.3.9
 
 fn main() {
-    let url = isahc::get("https://google.com");
-    println!("{:?}", url);
+    let payload = "<body onbeforeprint=alert(1)>";
+    let url = "https://portswigger-labs.net/xss/xss.php?x=$";
+    let re = Regex::new("\\$").unwrap();
+    let url2 = re.replace(url, payload);
+    println!("{:?}", url2); // This prints https://portswigger-labs.net/xss/xss.php?x=<body onbeforeprint=alert(1)>
 }
