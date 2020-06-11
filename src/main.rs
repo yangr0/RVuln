4a5a2d0b1a4f7052605f09e72e59eb200fa9e579
iinc0gnit0 <iinc0gnit0@pm.me>
2020-06-11 10:50:48 -0500

    0.0.1 - inc0gnit0

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+// Created by inc0gnit0, skript0r, ElementalX, p4yl0ad
+// Latest update: 6/11/20
+// Version: 0.0.1
+
 // Dependencies
 use isahc; // 0.9.3
 use regex::Regex; // 1.3.9
 
+// Main
 fn main() {
     let payload = "<body onbeforeprint=alert(1)>";
-    let url = "https://portswigger-labs.net/xss/xss.php?x=$";
+    let target = "https://portswigger-labs.net/xss/xss.php?x=$";
+    // This regex below replaces $ with the payload
     let re = Regex::new("\\$").unwrap();
-    let url2 = re.replace(url, payload);
-    println!("{:?}", url2); // This prints https://portswigger-labs.net/xss/xss.php?x=<body onbeforeprint=alert(1)>
+    let new_url = re.replace(target, payload);
+    println!("{:?}", new_url); // This prints https://portswigger-labs.net/xss/xss.php?x=<body onbeforeprint=alert(1)>
 }
