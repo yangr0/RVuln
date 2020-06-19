8b2d188a1628ece264e18c36ba211117d0262713
iinc0gnit0 <iinc0gnit0@pm.me>
2020-06-19 18:19:42 -0500

    v0.0.3 - Able to check for xss

--- CHANGLOG.md
+++ CHANGLOG.md
@@ -4,3 +4,6 @@
 # v0.0.2
 - Added read from wordlist
 - Added multi-threading
+
+# v0.0.3
+- Able to check for xss
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
-// Created by inc0gnit0, skript0r, ElementalX, p4yl0ad
-// Latest update: 6/11/20
-// Version: 0.0.1
+// Created by inc0gnit0, skript0r, p4yl0ad
+// Latest update: 6/19/20
+// Version: 0.0.3
 
 // Dependencies
 use isahc::prelude::*; // 0.9.3
-use rayon::prelude::*; // 1.3.0
+use rayon::prelude::*;
 use regex::Regex; // 1.3.9
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::time::Duration;
 
-// Read from file
+// Main
 fn main() -> std::io::Result<()> {
-    let url = "https://portswigger-labs.net/xss/xss.php?x=$";
+    // Check internet connection
+    match connection() {
+        Ok(send) => send,
+        Err(_) => panic!("\x1b[91mConnection not found!\x1b[0m"),
+    }
     let wordlist = "default.txt";
+    let target_host = "https://portswigger-labs.net/xss/xss.php?x=$";
+    let ua = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36";
+    let verbose = 1;
+    let timeout = 15;
+    // Reads payloads from file
     let mut payloads: Vec<String> = Vec::new();
     let fd = File::open(wordlist)?;
     for payload in BufReader::new(fd).lines() {
@@ -20,36 +30,50 @@ fn main() -> std::io::Result<()> {
         let payload = payload.trim().to_owned();
         payloads.push(payload);
     }
-    payloads
-        .par_iter()
-        .for_each(|url_path| match request(url, url_path) {
+    // Multi-threaded request
+    payloads.par_iter().for_each(|url_path| {
+        match request(target_host, url_path, ua, verbose, timeout) {
             Ok(request) => request,
-            Err(e) => println!("{}", e),
-        });
+            Err(e) => println!("\x1b[91mSomething went wrong!\nError: {}", e),
+        }
+    });
 
     Ok(())
 }
 
 // XSS
-fn request(host: &str, payload: &str) -> Result<(), Box<dyn std::error::Error>> {
-    // Config values
-    let ua = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36";
+fn request(
+    host: &str,
+    payload: &str,
+    ua: &str,
+    verbose: i8,
+    timeout: u64,
+) -> Result<(), Box<dyn std::error::Error>> {
     // This regex below replaces $ with the payload
     let re = Regex::new("\\$").unwrap();
     let url = re.replace(host, payload);
     let url = url_encode(&url);
     // Sends request
-    let request = Request::get(url)
+    let request = Request::get(url.clone())
         .header("user-agent", ua)
+        .timeout(Duration::new(timeout, 0))
         .body(())?
         .send()?
         .text()?;
 
     let source = request.contains(payload); // Check if source code contains payload
-    if source == true {
-        println!("Website is vulnerable!")
-    } else if source == false {
-        println!("Website is not vulnerble")
+    if verbose == 0 {
+        if source == true {
+            println!("\x1b[92m[+] {}", url)
+        } else if source == false {
+            print!("")
+        }
+    } else if verbose == 1 {
+        if source == true {
+            println!("\x1b[92m[+] {}", url)
+        } else if source == false {
+            println!("\x1b[91m[-] {}", url)
+        }
     }
 
     Ok(())
@@ -77,3 +101,19 @@ fn url_encode(data: &str) -> String {
     }
     return buffer;
 }
+fn connection() -> Result<(), Box<dyn std::error::Error>> {
+    Request::head("https://github.com")
+        .timeout(Duration::new(15, 0))
+        .body("")?
+        .send()?;
+
+    Ok(())
+}
+/*
+red \x1b[91m
+green \x1b[92m
+yellow \x1b[93m
+blue \x1b[94m
+magenta \x1b[95m
+reset \x1b[0m
+*/
