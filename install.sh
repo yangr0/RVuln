b74659ce2a6bdb7bd31e07b2a35864414a921b56
inc0gnit0 <iinc0gnit0@pm.me>
2020-07-02 13:55:20 -0500

    v0.0.8 - Few touch ups

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl https://sh.rustup.rs > rust.sh # Download Rust installation
-chmod +x rust.sh
-./rust.sh -y # Runs the installation file
-rm rust.sh # Removes the installation file
-cargo build # Compiles the code
-mv target/debug/RVuln RVuln # Moves the executable to RBust directory
